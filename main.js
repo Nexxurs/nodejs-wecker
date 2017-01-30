d161f0254f9d1dd7d6575ea7a90221cf58a8b7ef
Nexxurs <p.uebermasser@gmx.at>
2017-01-30 21:51:49 +0100

    First Commit
    WIP basic functions implemented
    No Reaction when alarm fires

---
+++ main.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var alarm = require('./lib/alarm').Alarm();
+
+
+var url = require('url');
+
+var debugmode = false;
+
+function debug(str) {
+	if (debugmode) {
+		console.log(str);
+	}
+}
+
+if (process.argv.length > 2) {
+	for(i=2; i<process.argv.length;i++){
+		switch(process.argv[i]){
+			case "debug":
+			case "-d":
+				debugmode = true
+				console.log("Debugmode on")
+		}
+	}
+}
+
+server.listen(8081, function () {
+	console.log('Server running on port '+server.address().port);
+});
+
+server.on('request', function(req,res){
+	var path = url.parse(req.url).pathname;
+	debug("Request for "+path+" received!");
+
+});
+
+function parseTime(string) {
+	var time = string.split(':');
+
+	if (time.length == 1) {
+		time[1] = 0;
+	}
+
+	return time;
+}
+
+alarm.setCallback(function(){
+	io.sockets.emit('trigger');
+});
+
+io.sockets.on('connection', function(socket){
+	debug("New Websocket Connection")
+	socket.emit('init',{
+		nextAlarm: alarm.getNext(),
+		now: new Date().getTime()
+	});
+	
+	socket.on('set', function(data){
+		alarm.setTime.apply(alarm, parseTime(data.newAlarm));
+		io.sockets.emit('update', {
+			nextAlarm: alarm.getNext(),
+			now: new Date().getTime()
+		});
+	});
+	
+	socket.on('stop', function(data){
+		alarm.cancel();
+		io.sockets.emit('stop', {
+			nextAlarm: alarm.getNext(),
+			now: new Date().getTime()
+		});
+	});
+
+});
+
+app.use(express.static(__dirname + '/html'));
